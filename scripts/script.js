ef13f4fea0c18d2f362f0df5badf01777e2ae15b
Will O'Beirne <wbobeirne@gmail.com>
2019-05-29 13:44:19 -0400

    Provide static fallback for mobile. Adjust animation a bit.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
 $(function() {
-  // Background animation
-  VANTA.NET({
-    el: document.querySelector('.Hero'),
-    color: '#0984e3',
-    backgroundColor: '#020133',
-  });
+  let vanta;
+  function updateAnimation() {
+    if (!vanta && window.innerWidth >= 640) {
+      vanta = VANTA.NET({
+        el: document.querySelector('.Hero'),
+        color: '#0984e3',
+        backgroundColor: '#020133',
+        mouseCoeffX: 0.4,
+        mouseCoeffY: -6,
+      });
+    } else if (vanta && window.innerWidth < 640) {
+      vanta.destroy();
+      vanta = null;
+    }
+  }
+
+  window.addEventListener('resize', updateAnimation);
+  window.addEventListener('focus', updateAnimation);
+  updateAnimation();
 });
